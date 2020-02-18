2c988619351f29efa51218cbc37ec1835f4b23b1
W0pkr1 <48027904+W0pkr1@users.noreply.github.com>
2020-02-18 19:53:38 +0100

    Create stepping.py

---
+++ stepping.py
@@ -0,0 +1,14 @@
+for i in range (9):
+    print('\n')
+    if (i-8 >=0):
+        i=i-8
+        print(8)
+    if (i-4 >=0):
+        i=i-4
+        print(4)
+    if (i-2 >=0):
+        i=i-2
+        print(2)
+    if (i-1 >=0):
+        i=i-1
+        print(1)
